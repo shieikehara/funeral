ab1ad1add9557ed975d68ee5a45d8f0d0e774404
shieikehara <s.ikehara.carecon@gmail.com>
2023-02-22 21:05:28 +0900

    sqlファイルの変更

--- resources/sql/funeral.sql
+++ resources/sql/funeral.sql
@@ -1,54 +1,67 @@
 
-CREATE TABLE users (
-    'id' int(11) NOT NULL,
-    'name' varchar(50) NOT NULL,
-    'tel' varchar(11) NOT NULL,
-    'email' varchar(100) NOT NULL,
-    'password' varchar(100) NOT NULL,
-    'funeral_id' int(11) NOT NULL,
-    'admin' int(11) NOT NULL DEFAULT 0   
+CREATE TABLE `users` (
+    `id` int(11) NOT NULL,
+    `name` varchar(50) NOT NULL,
+    `tel` varchar(11) NOT NULL,
+    `email` varchar(100) NOT NULL,
+    `password` varchar(100) NOT NULL,
+    `funeral_id` int(11) NOT NULL,
+    `admin` int(11) NOT NULL DEFAULT 0   
 );
 
-CREATE TABLE funerals (
-    'id' int(11) NOT NULL,
-    'family_name' varchar(50) NOT NULL,
-    'deceased' varchar(50) NOT NULL,
-    'name' varchar(50) NOT NULL,
-    'day1' varchar(50),
-    'day2' varchar(50),
-    'dinner' int(11),
-    'lunch' int(11),
-    'funeral_style' varchar(50) NOT NULL,
-    'created_at' datetime,
-    'updated_at' datetime
+CREATE TABLE `funerals` (
+    `id` int(11) NOT NULL,
+    `family_name` varchar(50) NOT NULL,
+    `deceased` varchar(50) NOT NULL,
+    `name` varchar(50) NOT NULL,
+    `day1` varchar(50),
+    `day2` varchar(50),
+    `dinner` int(11),
+    `lunch` int(11),
+    `funeral_style` varchar(50) NOT NULL,
+    `created_at` datetime,
+    `updated_at` datetime
 );
 
-CREATE TABLE flowers (
-    'id' int(11) NOT NULL,
-    'name' varchar(50) NOT NULL,
-    'tel' varchar(11) NOT NULL,
-    'nameplate' varchar(100) NOT NULL,
-    'flower' varchar(50) NOT NULL,
-    'volume' int(11) NOT NULL,
-    'funeral_id' int(11) NOT NULL,
-    'created_at' datetime
+INSERT INTO `funerals` (`id`, `family_name`, `deceased`, `name`, `day1`, `day2`, `dinner`, `lunch`, `funeral_style`, `created_at`, `updated_at`)
+VALUES
+(1, '山田家', '山田太郎', '山田次郎', '2023-01-01', '2023-01-02', 10, 10, '2日間仏式', now(), now());
+
+
+CREATE TABLE `flowers` (
+    `id` int(11) NOT NULL,
+    `name` varchar(50) NOT NULL,
+    `tel` varchar(11) NOT NULL,
+    `nameplate` varchar(100) NOT NULL,
+    `flower` varchar(50) NOT NULL,
+    `volume` int(11) NOT NULL,
+    `funeral_id` int(11) NOT NULL,
+    `created_at` datetime
 );
 
-CREATE TABLE u_flowers (
-    'id' int(11) NOT NULL,
-    'user_id' int(11) NOT NULL,
-    'flower_id' int(11) NOT NULL,
-    'created_at' datetime
+CREATE TABLE `u_flowers` (
+    `id` int(11) NOT NULL,
+    `user_id` int(11) NOT NULL,
+    `flower_id` int(11) NOT NULL,
+    `created_at` datetime
 );
 
-CREATE TABLE forms (
-    'id' int(11) NOT NULL,
-    'name' varchar(50) NOT NULL,
-    'tel' varchar(11),
-    'mail' varchar(100) NOT NULL,
-    'body' varchar(500) NOT NULL,
-    'created_at' datetime,
-    'updated_at' datetime
+CREATE TABLE `forms` (
+    `id` int(11) NOT NULL,
+    `name` varchar(50) NOT NULL,
+    `tel` varchar(11),
+    `mail` varchar(100) NOT NULL,
+    `body` varchar(500) NOT NULL,
+    `created_at` datetime,
+    `updated_at` datetime
 );
 
-ALTER TABLE users ADD CONSTRAINT users_pkey PRIMARY KEY(id);
+ALTER TABLE `users` ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `funerals` ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `flowers` ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `u_flowers` ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `forms` ADD PRIMARY KEY (`id`);
